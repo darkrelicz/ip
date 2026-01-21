521bcb56687375e83adfe4013e6a96346c8aa14f
darkrelicz <zifengtang540@gmail.com>
2026-01-21 22:36:12 +0800

    Added hello and goodbye message

---
+++ src/main/java/Dawn.java
@@ -0,0 +1,17 @@
+public class Dawn {
+    public static void main(String[] args) {
+        String logo = " ____                        \n"
+            + "|  _ \\   __ _ __      __ _ __ \n"
+            + "| | | | / _` |\\ \\ /\\ / /| '_ \\ \n"
+            + "| |_| || (_| | \\ V  V / | | | |\n"
+            + "|____/  \\__,_|  \\_/\\_/  |_| |_|\n";
+        System.out.println("Hello from\n" + logo);
+
+        System.out.println("""
+        Hello! I'm Dawn
+        What can I do for you?
+
+        Bye. Hope to see you again soon!
+        """);
+    }
+}
