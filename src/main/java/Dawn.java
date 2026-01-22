3b95185b386935e3b8f523b4f9eaf0c119b42143
DarkRelicz <zifengtang540@gmail.com>
2026-01-22 23:13:20 +0800

    Automated text UI testing

--- src/main/java/Dawn.java
+++ src/main/java/Dawn.java
@@ -3,6 +3,12 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Dawn {
+    private static void printTask(Task newTask, ArrayList<Task> db) {
+        System.out.println("  Got it. I've added this task:");
+        System.out.println("  "+newTask.toString());    
+        System.out.println(String.format("  Now you have %d tasks in the list.", db.size()));
+    }
+
     public static void main(String[] args) {
         ArrayList<Task> db = new ArrayList<>();
         Scanner s = new Scanner(System.in);
@@ -20,67 +26,59 @@ public class Dawn {
             String body = String.join(" ", Arrays.copyOfRange(parts, 1, parts.length));
             switch (cmd.toLowerCase()) {
                 case "bye":
-                    System.out.println("\tBye. Hope to see you again soon!");
+                    System.out.println("  Bye. Hope to see you again soon!");
                     exit = true;    
                     break; 
 
                 case "list":
-                    System.out.println("\tHere are the tasks in your list:");
+                    System.out.println("  Here are the tasks in your list:");
                     for (int i = 0; i < db.size(); i++) {
                         String res = String.format("%d. %s", i+1, db.get(i).toString());
-                        System.out.println("\t"+res);
+                        System.out.println("  "+res);
                     }     
                     break;
 
                 case "mark":  
                     if (parts.length < 2) {
-                        System.out.println("\tusage: mark <task number>");
+                        System.out.println("  usage: mark <task number>");
                         break;
                     }
                     int taskNum = Integer.parseInt(parts[1])-1;
                     db.get(taskNum).markDone();
-                    System.out.println("\tNice! I've marked this task as done:");
-                    System.out.println("\t"+db.get(taskNum).toString());
+                    System.out.println("  Nice! I've marked this task as done:");
+                    System.out.println("  "+db.get(taskNum).toString());
                     break;
 
                 case "unmark":  
                     if (parts.length < 2) {
-                        System.out.println("\tusage: unmark <task number>");
+                        System.out.println("  usage: unmark <task number>");
                         break;
                     }
                     int taskIndex = Integer.parseInt(parts[1])-1;
                     db.get(taskIndex).unmarkDone();
-                    System.out.println("\tOK, I've marked this task as not done yet:");
-                    System.out.println("\t"+db.get(taskIndex).toString());
+                    System.out.println("  OK, I've marked this task as not done yet:");
+                    System.out.println("  "+db.get(taskIndex).toString());
                     break;
 
                 case "todo":
                     if (body.length() == 0) {
-                        System.out.println("\tusage: todo <task description>");
+                        System.out.println("  usage: todo <task description>");
                         break;
                     }
                     Task todo = new Todo(body);
                     db.add(todo);
-                    System.out.println(String.format("""
-                        \tGot it. I've added this task:
-                        \t%s
-                        \tNow you have %d tasks in the list.""", 
-                        todo.toString(), db.size()));
+                    printTask(todo, db);
                     break;
 
                 case "deadline":
                     String[] deadlineParts = body.split(" /by ");
                     if (deadlineParts.length < 2) {
-                        System.out.println("\tusage: deadline <task> /by <deadline>");
+                        System.out.println("  usage: deadline <task> /by <deadline>");
                         break;
                     }
                     Task deadline = new Deadline(deadlineParts[0], deadlineParts[1]); 
                     db.add(deadline);
-                    System.out.println(String.format("""
-                        \tGot it. I've added this task:
-                        \t%s
-                        \tNow you have %d tasks in the list.""", 
-                        deadline.toString(), db.size()));
+                    printTask(deadline, db); 
                     break;
 
                 case "event":
@@ -92,23 +90,18 @@ public class Dawn {
                     String eventDesc = eventParts[0];
                     String[] dates = eventParts[1].split(" /to ");
                     if (dates.length < 2) {
-                        System.out.println("\tusage: event <task> /from <start date> /to <end date>");
+                        System.out.println("  usage: event <task> /from <start date> /to <end date>");
                         break;
                     }
                     String start = dates[0];
                     String end = dates[1];
-
                     Task event = new Event(eventDesc, start, end); 
                     db.add(event);
-                    System.out.println(String.format("""
-                        \tGot it. I've added this task:
-                        \t%s
-                        \tNow you have %d tasks in the list.""", 
-                        event.toString(), db.size()));
+                    printTask(event, db);
                     break;
                     
                 default:
-                    System.out.println("\tUnknown command, try again.");
+                    System.out.println("  Unknown command, try again.");
                     break;
             }
            
